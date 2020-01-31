a59f2e15390a6e4df405b0bb1aa2cc827c4e38c1
Okoro Ikechukwu <ikeshegs@gmail.com>
2020-01-31 13:37:00 +0100

    modify rate limit settings

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -3,11 +3,9 @@ const rateLimit = require("express-rate-limit");
 
 
 const limiter = rateLimit({
-  windowMs: 60000,
-  max: process.env.CALL_PER_MINUTE || 10,
-  message: {
-    error: "Too many requests"
-  }
+  windowMs: 60 * 15 * 1000,
+  max: process.env.CALL_PER_MINUTE,
+  message: "Too many requests from this IP, please try again after 15 minutes"
 });
 
 module.exports = limiter;
