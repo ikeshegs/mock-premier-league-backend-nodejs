147bab80e275b268d502160140313e7476fdfc09
Okoro Ikechukwu <ikeshegs@gmail.com>
2020-02-01 12:36:35 +0100

    change redis cloud settings

--- middlewares/session.js
+++ middlewares/session.js
@@ -5,15 +5,15 @@ const redisStore = require('connect-redis')(session);
 const client = redis.createClient();
 
 const redisSession = session({
-  secret: process.env.REDIS_SECRET || process.env.REDIS_CLOUD_PASSWORD,
+  secret: process.env.REDIS_SECRET,
   store: new redisStore({
-    host: process.env.REDIS_CLOUD_HOST || 'localhost',
+    host: 'localhost',
     port: 6379,
     client: client,
     ttl: 1000
   }),
   saveUninitialized: false,
   resave: false
-});
+} || process.env.REDIS_CLOUD_HOST);
 
 module.exports = redisSession;
